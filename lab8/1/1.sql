f699169d4c8d6da3320cac7eb40142bd76a247dd
FewPz <sharpkung4826@gmail.com>
2024-02-08 22:19:19 +0700

    feat: lab8 done

---
+++ lab8/1/1.sql
@@ -0,0 +1,453 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 
+-- Generation Time: Feb 08, 2024 at 03:15 PM
+-- Server version: 8.3.0
+-- PHP Version: 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+07:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `advisor`
+--
+
+CREATE TABLE `advisor` (
+  `s_ID` int NOT NULL,
+  `i_ID` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `advisor`
+--
+
+INSERT INTO `advisor` (`s_ID`, `i_ID`) VALUES
+(12345, 45565),
+(70557, 76543),
+(45678, 76766),
+(98988, 76766),
+(19991, 98345);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classroom`
+--
+
+CREATE TABLE `classroom` (
+  `building` varchar(255) NOT NULL,
+  `room_number` int NOT NULL,
+  `capacity` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `classroom`
+--
+
+INSERT INTO `classroom` (`building`, `room_number`, `capacity`) VALUES
+('Packard', 101, 500),
+('Painter', 514, 10),
+('Taylor', 3128, 70),
+('Watson', 100, 30);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` varchar(8) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `dept_name` varchar(255) NOT NULL,
+  `credit` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_id`, `title`, `dept_name`, `credit`) VALUES
+('BIO-101', 'Intro. to Biology', 'Biology', 4),
+('BIO-301', 'Genetics', 'Biology', 4),
+('BIO-999', 'Computational Biology', 'Biology', 3),
+('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', 4),
+('CS-190', 'Game Design', 'Comp. Sci.', 4),
+('CS-315', 'Robotics', 'Comp. Sci.', 3),
+('CS-319', 'Image Processing', 'Comp. Sci.', 3),
+('CS-347', 'Database System Concepts', 'Comp. Sci.', 3),
+('EE-181', 'Intro. to Digital System', 'Elec. Eng.', 3),
+('FIN-201', 'Investment Banking', 'Finance', 3),
+('HIS-351', 'World History', 'History', 3),
+('MU-199', 'Music Video Production', 'Music', 3),
+('PHY-101', 'Physical Principles', 'Physics', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `dept_name` varchar(255) NOT NULL,
+  `building` varchar(255) NOT NULL,
+  `budget` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`dept_name`, `building`, `budget`) VALUES
+('Biology', 'Watson', 90000),
+('Comp. Sci.', 'Taylor', 100000),
+('Elec. Eng.', 'Taylor', 85000),
+('Finance', 'Painter', 120000),
+('History', 'Painter', 50000),
+('Music', 'Packard', 80000),
+('Physics', 'Watson', 70000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructor`
+--
+
+CREATE TABLE `instructor` (
+  `ID` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `dept_name` varchar(255) NOT NULL,
+  `salary` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `instructor`
+--
+
+INSERT INTO `instructor` (`ID`, `name`, `dept_name`, `salary`) VALUES
+(10101, 'Srinivasan', 'Comp. Sci.', 65000),
+(12121, 'Wu', 'Finance', 90000),
+(15151, 'Mozart', 'Music', 40000),
+(22222, 'Einstein', 'Physics', 95000),
+(32343, 'El Said', 'History', 60000),
+(33456, 'Gold', 'Physics', 87000),
+(45565, 'Katz', 'Comp. Sci.', 75000),
+(58583, 'Califieri', 'History', 62000),
+(76543, 'Singh', 'Finance', 80000),
+(76766, 'Crick', 'Biology', 72000),
+(83821, 'Brandt', 'Comp. Sci.', 92000),
+(98345, 'Kim', 'Elec. Eng.', 80000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prereq`
+--
+
+CREATE TABLE `prereq` (
+  `course_id` varchar(9) NOT NULL,
+  `prereq_id` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `prereq`
+--
+
+INSERT INTO `prereq` (`course_id`, `prereq_id`) VALUES
+('BIO-301', 'BIO-101'),
+('BIO-399', 'BIO-101'),
+('CS-315', 'BIO-101'),
+('CS-190', 'CS-101'),
+('CS-319', 'CS-101'),
+('CS-347', 'CS-101'),
+('EE-181', 'PHY-101');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `section`
+--
+
+CREATE TABLE `section` (
+  `course_id` varchar(9) NOT NULL,
+  `sec_id` int NOT NULL,
+  `semester` set('Summer','Fall','Spring') NOT NULL,
+  `year` int NOT NULL,
+  `building` varchar(255) NOT NULL,
+  `room_number` int NOT NULL,
+  `time_slot_id` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `section`
+--
+
+INSERT INTO `section` (`course_id`, `sec_id`, `semester`, `year`, `building`, `room_number`, `time_slot_id`) VALUES
+('BIO-101', 1, 'Summer', 2017, 'Painter', 514, 'B'),
+('CS-101', 1, 'Fall', 2017, 'Packard', 101, 'D'),
+('CS-315', 1, 'Spring', 2018, 'Watson', 120, 'D'),
+('EE-181', 1, 'Spring', 2017, 'Taylor', 3128, 'C'),
+('PHY-101', 1, 'Fall', 2017, 'Watson', 100, 'A');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `ID` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `dept_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `tot_cred` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`ID`, `name`, `dept_name`, `tot_cred`) VALUES
+(12345, 'Shankar', 'Comp. Sci.', 32),
+(19991, 'Brandt', 'History', 80),
+(45678, 'Levy', 'Physics', 46),
+(70557, 'Snow', 'Physics', 0),
+(98988, 'Tanaka', 'Biology', 120);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `takes`
+--
+
+CREATE TABLE `takes` (
+  `ID` int NOT NULL,
+  `course_id` varchar(9) NOT NULL,
+  `sec_id` int NOT NULL,
+  `semester` enum('Spring','Fall','Summer') NOT NULL,
+  `year` int NOT NULL,
+  `grade` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `takes`
+--
+
+INSERT INTO `takes` (`ID`, `course_id`, `sec_id`, `semester`, `year`, `grade`) VALUES
+(12345, 'CS-101', 1, 'Fall', 2017, 'C'),
+(19991, 'HIS-351', 1, 'Spring', 2018, 'B'),
+(45678, 'CS-101', 1, 'Spring', 2018, 'B+'),
+(70557, 'CS-315', 1, 'Spring', 2018, 'B'),
+(98988, 'BIO-301', 1, 'Summer', 2018, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teaches`
+--
+
+CREATE TABLE `teaches` (
+  `ID` int NOT NULL,
+  `course_id` varchar(9) NOT NULL,
+  `sec_id` int NOT NULL,
+  `semester` enum('Fall','Spring','Summer') NOT NULL,
+  `year` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `teaches`
+--
+
+INSERT INTO `teaches` (`ID`, `course_id`, `sec_id`, `semester`, `year`) VALUES
+(10101, 'CS-101', 1, 'Fall', 2017),
+(12121, 'CS-347', 1, 'Spring', 2018),
+(15151, 'MU-199', 1, 'Spring', 2018),
+(32343, 'HIS-351', 1, 'Spring', 2018),
+(98345, 'EE-181', 1, 'Spring', 2017);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_slot`
+--
+
+CREATE TABLE `time_slot` (
+  `time_slot_id` varchar(1) NOT NULL,
+  `day` varchar(1) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `time_slot`
+--
+
+INSERT INTO `time_slot` (`time_slot_id`, `day`, `start_time`, `end_time`) VALUES
+('A', 'M', '08:00:00', '08:50:00'),
+('B', 'M', '09:00:00', '09:50:00'),
+('C', 'W', '11:00:00', '11:50:00'),
+('D', 'F', '13:00:00', '13:50:00'),
+('E', 'T', '10:30:00', '11:45:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `advisor`
+--
+ALTER TABLE `advisor`
+  ADD PRIMARY KEY (`s_ID`),
+  ADD KEY `advisor_instructor` (`i_ID`);
+
+--
+-- Indexes for table `classroom`
+--
+ALTER TABLE `classroom`
+  ADD PRIMARY KEY (`building`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`),
+  ADD KEY `course_dept_name_fk` (`dept_name`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`dept_name`);
+
+--
+-- Indexes for table `instructor`
+--
+ALTER TABLE `instructor`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `instructor_dept_name_fk` (`dept_name`);
+
+--
+-- Indexes for table `prereq`
+--
+ALTER TABLE `prereq`
+  ADD PRIMARY KEY (`course_id`),
+  ADD KEY `prereq_course_id_ffk` (`prereq_id`);
+
+--
+-- Indexes for table `section`
+--
+ALTER TABLE `section`
+  ADD PRIMARY KEY (`course_id`),
+  ADD KEY `section_building` (`building`),
+  ADD KEY `section_time_slot_id` (`time_slot_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `student_department` (`dept_name`);
+
+--
+-- Indexes for table `takes`
+--
+ALTER TABLE `takes`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `takes_course_id` (`course_id`);
+
+--
+-- Indexes for table `teaches`
+--
+ALTER TABLE `teaches`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `time_slot`
+--
+ALTER TABLE `time_slot`
+  ADD PRIMARY KEY (`time_slot_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `advisor`
+--
+ALTER TABLE `advisor`
+  MODIFY `s_ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98989;
+
+--
+-- AUTO_INCREMENT for table `instructor`
+--
+ALTER TABLE `instructor`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98346;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `advisor`
+--
+ALTER TABLE `advisor`
+  ADD CONSTRAINT `advisor_instructor` FOREIGN KEY (`i_ID`) REFERENCES `instructor` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `advisor_student` FOREIGN KEY (`s_ID`) REFERENCES `student` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `course`
+--
+ALTER TABLE `course`
+  ADD CONSTRAINT `course_dept_name_fk` FOREIGN KEY (`dept_name`) REFERENCES `department` (`dept_name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `instructor`
+--
+ALTER TABLE `instructor`
+  ADD CONSTRAINT `instructor_dept_name_fk` FOREIGN KEY (`dept_name`) REFERENCES `department` (`dept_name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `section`
+--
+ALTER TABLE `section`
+  ADD CONSTRAINT `section_building` FOREIGN KEY (`building`) REFERENCES `classroom` (`building`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `section_course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `section_time_slot_id` FOREIGN KEY (`time_slot_id`) REFERENCES `time_slot` (`time_slot_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_department` FOREIGN KEY (`dept_name`) REFERENCES `department` (`dept_name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `takes`
+--
+ALTER TABLE `takes`
+  ADD CONSTRAINT `take_id` FOREIGN KEY (`ID`) REFERENCES `student` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `takes_course_id` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `teaches`
+--
+ALTER TABLE `teaches`
+  ADD CONSTRAINT `teaches` FOREIGN KEY (`ID`) REFERENCES `instructor` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
